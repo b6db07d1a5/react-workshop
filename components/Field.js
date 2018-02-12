44c49290831d283c6ea49b7b686f58262d1aac80
Sitthichai Wanthong <sitthichaiw@Ananass-MacBook-Pro.local>
2018-02-12 10:06:51 +0700

    refactor ; all file

--- components/Field.js
+++ components/Field.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Row from './Row'
+import Row from './Row';
 
 const Field = ({field, randomMove}) => {
     return (
--- components/Tank.js
+++ components/Tank.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Field from './Field'
+import Field from './Field';
 
 const DIRECTION = {
     1: 'up',
